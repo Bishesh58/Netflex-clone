04438115fd2441afc0f0712fcfd0975c1dc8f9a2
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-10-10 02:35:14 +1300

    fetching movies according to genres completed

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+
+const API_KEY = "19f84e11932abbc79e6d83f82d6d1045";
+
 window.onload = () => {
   getOriginals();
   getTrendingNow();
   getTopRated();
   getAction();
+  getHorror();
+  getSiFi();
+  getRomance();
+  getComedies();
+  getDocumentaries();
+  getFantasy();
+  getThriller();
+  getWestern();
+  getDrama();
+  getMistery();
+  getCrime();
 };
 
 function showMovies(movies, element_selector, path_type) {
@@ -34,62 +48,84 @@ function fetchMovies(url, element_selector, path_type) {
 
 function getOriginals() {
   var url =
-    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
+    `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`;
   fetchMovies(url, ".original__movies", "poster_path");
 }
 
 function getTrendingNow() {
   var url =
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045";
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
   fetchMovies(url, ".movies__containerTrending", "backdrop_path");
 }
 function getTopRated() {
   var url =
-    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
   fetchMovies(url, ".movies__containerRated", "backdrop_path");
 }
 
+
+
 function getAction() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=28`;
   fetchMovies(url, ".movies__containerAction", "backdrop_path");
 }
 
 function getHorror() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=27`;
   fetchMovies(url, ".movies__containerHorror", "backdrop_path");
 }
 
 function getSiFi() {
-  var url = "";
-  fetchMovies(url, ".movies__containerScienceFiction", "backdrop_path");
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=878`;
+  fetchMovies(url, ".movies__containerSiFi", "backdrop_path");
 }
 
 function getRomance() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=10749`;
   fetchMovies(url, ".movies__containerRomance", "backdrop_path");
 }
 
 function getComedies() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=35`;
   fetchMovies(url, ".movies__containerComedies", "backdrop_path");
 }
 
 function getAnimation() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=16`;
   fetchMovies(url, ".movies__containerAnimation", "backdrop_path");
 }
 
+function getMistery() {
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=9648`;
+  fetchMovies(url, ".movies__containerMistery", "backdrop_path");
+}
+
 function getDocumentaries() {
-  var url = "";
+  var url =`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=99`;
   fetchMovies(url, ".movies__containerDocumentaries", "backdrop_path");
 }
 
 function getFantasy() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=14`;
   fetchMovies(url, ".movies__containerFantasy", "backdrop_path");
 }
 
 function getThriller() {
-  var url = "";
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=53`;
   fetchMovies(url, ".movies__containerThriller", "backdrop_path");
+}
+
+function getDrama() {
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=18`;
+  fetchMovies(url, ".movies__containerDrama", "backdrop_path");
+}
+
+function getWestern() {
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=37`;
+  fetchMovies(url, ".movies__containerWestern", "backdrop_path");
+}
+
+function getCrime() {
+  var url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=80`;
+  fetchMovies(url, ".movies__containerCrime", "backdrop_path");
 }
