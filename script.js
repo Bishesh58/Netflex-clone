06512fff4351c581b5fa47956cbb8ecbcb33b5f8
Bishesh <bishesh.sunam@gmail.com>
2020-10-30 22:55:21 +1300

    added fetch funtions on genres

--- script.js
+++ script.js
@@ -50,14 +50,15 @@ function showMovies(movies, element_selector, path_type) {
   
   getMovieTrailer(id).then((data)=>{
    const results = data.results;
+   console.log(results);
    const youtubeTrailers = results.filter((result)=>{
-     if(results.site == "Youtube" && results.type == "Trailer"){
+     if(result.site == "YouTube" && result.type == "Trailer"){
        return true;
      } else{
        return false;
      }
    })
-
+   setTrailer(youtubeTrailers);
   })
 
   $('#trailer').modal('show')
@@ -83,13 +84,17 @@ async function getMovieTrailer(id){
 
 const setTrailer = (trailers)=>{
   const iframe = document.getElementById('movieTrailer');
+  const movieNotFound = document.querySelector(".hide");
   if(trailers.length > 0 ){
+    movieNotFound.classList.add('d-none')
+    iframe.classList.remove('d-none');
     iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`
+  } else{
+    iframe.classList.toggle('d-none');
+    movieNotFound.classList.remove('d-none')
   }
+
 }
-
-
-
 
 function getOriginals() {
   var url =
@@ -100,17 +105,19 @@ function getOriginals() {
 function getTrendingNow() {
   var url =
     `${baseURL}/trending/movie/week?api_key=${API_KEY}`;
-  fetchMovies(url, ".movies__containerTrending", "poster_path");
+  fetchMovies(url, ".trending__movies", "poster_path");
 }
+
 function getTopRated() {
   var url =
     `${baseURL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
-  fetchMovies(url, ".movies__containerRated", "poster_path");
+  fetchMovies(url, ".topRated__movies", "poster_path");
 }
 
 
 fetchgenres =()=>{
   url = `${baseURL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+  
   fetch(url)
   .then((response) =>{
     return response.json();
@@ -124,9 +131,27 @@ fetchgenres =()=>{
 }
 
 showGenres =(movies)=>{
-movies.genres.map((genre) =>{
-  let url = `${baseURL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre.id}`
+
+  let moviesContainer = document.querySelector(".movies__container");
+
+ movies.genres.map((genre) =>{
+
+  let titleEl = document.createElement('div');
+  let moviesList = document.createElement('div');
+
+  moviesList.classList.add('.movies__genres');
+  titleEl.classList.add('movies__title');
+
+  moviesContainer.append(titleEl);
+  moviesContainer.append(moviesList);
+
+  console.log(moviesContainer);
+
   let movieName = genre.name.replace(/\s+/g, "");
-  fetchMovies(url, `.movies__container${movieName}`, "poster_path")
+  titleEl.innerHTML += `<h2>${movieName}</h2>`;
+  
+
+  let url = `${baseURL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre.id}`
+  fetchMovies(url, ".movies__genres", "poster_path")
  })
 }
