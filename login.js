5f3421b427a293a9e84373cc963e38152accf3fa
Bishesh <bishesh.sunam@gmail.com>
2020-12-04 23:48:52 +1300

    login data fetched from mongodb

--- login.js
+++ login.js
@@ -1,13 +1,33 @@
 
 let loginForm = document.getElementById("login__form");
-let apiUrl = 
+let apiUrl = 'http://localhost:3000';
 
 
 
 loginForm.addEventListener("submit", (event)=>{
     event.preventDefault();
    
+    let payload = {
+        email: document.getElementById("email").value,
+        passowrd: document.getElementById("password").value
+    }
+
+
     fetch(apiUrl + "/login", {
         method: "POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        } else{
+            throw new Error ("something went wrong");
+        }
+    })
+    .then((response) =>{
+        console.log(response);
     })
 })
