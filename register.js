dfb8e3e1f78db019c0fda4ebd36cb0e6cec018f5
Bishesh <bishesh.sunam@gmail.com>
2020-12-07 23:50:41 +1300

    added register.js

---
+++ register.js
@@ -0,0 +1,34 @@
+let registerForm = document.getElementById("register__form");
+let apiUrl = 'http://localhost:3000';
+
+
+registerForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+   
+    let payload = {
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        passowrd: document.getElementById("password").value
+    }
+
+
+    fetch(apiUrl + "/register", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then((response)=>{
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("something went wrong");
+        }
+    }) // returns a promise already
+    .then((response)=>{
+        localStorage.setItem('token', response.token)
+        location.href = "/";
+    })
+
+})
